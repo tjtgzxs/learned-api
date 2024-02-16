bb96163e5e5fe40d334bce834eeeadbc288b9295
Johnson <johnson27945@outlook.com>
2024-02-16 15:13:52 +1300

    add function

--- app/Http/Controllers/InformationController.php
+++ app/Http/Controllers/InformationController.php
@@ -17,19 +17,14 @@ class InformationController extends Controller
     public function index(Request $request)
     {
         if(!empty($request->category_id)&&$request->category_id!=0){
-            if(Cache::get('information_'.$request->category_id)){
-                return Cache::get('information_'.$request->category_id);
-            }
+
             $list=InformationResource::collection(Information::where('category_id', $request->category_id)->orderBy('id', 'desc')->get());
-            Cache::put('information_'.$request->category_id, $list, 60);
             return $list;
         }
-        if(Cache::get('information_all')){
-            return Cache::get('information_all');
-        }
+
 
         $list= InformationResource::collection(Information::orderBy('id', 'desc')->get());
-        Cache::put('information_all', $list, 60);
+
         return $list;
     }
 
